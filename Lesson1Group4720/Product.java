d23a3125f253383559f7bbf2ffab8cef17330ab3
Kingofhell1 <chernyakov_99@list.ru>
2023-08-26 13:23:32 +0300

    Added file OOP my group 4720

---
+++ Lesson1Group4720/Product.java
@@ -0,0 +1,25 @@
+package Lesson1Group4720;
+public class Product{
+    private String name;
+    private double price;
+    private int rating;
+
+    public Product(String name, double price, int rating){
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+
+    }
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public int getRating(){
+        return rating;
+    }
+
+}
